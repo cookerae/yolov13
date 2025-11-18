d74c7cf04a1c926259f1fe8b5edda1959fa7a69c
cookerae <cookstronekuoler@gmail.com>
2025-11-18 19:59:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov13l.yaml')
+model = YOLO('yolov13s.yaml')
 
 # Train the model
 results = model.train(
   data='/kaggle/input/US-needle/needle/data.yaml',
   epochs=600, 
-  batch=16, 
-  imgsz=640,
+  batch=32, 
+  imgsz=512,
   scale=0.9,  # S:0.9; L:0.9; X:0.9
   mosaic=1.0,
-  mixup=0.15,  # S:0.05; L:0.15; X:0.2
-  copy_paste=0.5,  # S:0.15; L:0.5; X:0.6
+  mixup=0.05,  # S:0.05; L:0.15; X:0.2
+  copy_paste=0.15,  # S:0.15; L:0.5; X:0.6
   device="0,1",
 )
 
